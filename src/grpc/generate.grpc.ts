826c8190f1a971f8e112697e29b1de9566da3914
loc van <locvt@innotech-vn.com>
2023-05-13 20:39:21 +0700

    a

--- src/grpc/generate.grpc.ts
+++ src/grpc/generate.grpc.ts
@@ -9,7 +9,7 @@ export const generateGRPCServer = (packageName: string, dir: string, port: numbe
         options: {
             url: `${hostname}:${port}`,
             package: packageName,
-            protoPath: join(dir, '..', `protos/${packageName}.proto`),
+            protoPath: join(dir, '..', `${packageName}/${packageName}.proto`),
         },
     } as GrpcOptions;
 };
